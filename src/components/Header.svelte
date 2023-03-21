5fd5d4d6baa9c82f4d74841c15807bcfa299cb3d
Agil Ghani Istikmal <agilistikmal3@gmail.com>
2023-03-21 22:55:24 +0700

    Landing page

---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+<div>
+	<h1 class="font-bold text-6xl">Chickrest</h1>
+	<h2 class="font-semibold text-xl">Chicken food restaurant.</h2>
+	<p>Book your table now!</p>
+	<button class="btn btn-1 mt-5">Book now</button>
+	<div class="absolute bottom-0 left-0 -z-1">
+		<lottie-player
+			src="https://assets10.lottiefiles.com/private_files/lf30_y1tasdqo.json"
+			background="transparent"
+			speed="0.3"
+			class="w-full"
+			loop
+			autoplay
+		/>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import Header from '../components/Header.svelte';
+</script>
+
+<div class="grid grid-cols-3">
+	<div class="from-red-800 to-red-500 bg-gradient-to-b min-h-screen text-white px-5 relative">
+		<div class="text-center mt-16">
+			<Header />
+		</div>
+	</div>
+	<main class="col-span-2 px-5">
+		<slot />
+	</main>
+</div>
